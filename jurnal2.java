241c358180b417940f071c4991ab23f86c098aae
alegrarsio <alegrarsio2005@gmail.com>
2023-09-26 11:51:31 +0700

    first commit

---
+++ jurnal2.java
@@ -0,0 +1,9 @@
+public class jurnal2 {
+    public static void main(String[] args){
+        String arr[] = {"alegrarsio gifta lesmana","607062300029","DF-IF-03"};
+        System.out.printf("%s",arr[0]);
+        System.out.println("Nim :",arr[1]);
+        System.out.println("Class :",arr[2]);
+
+    }
+}
